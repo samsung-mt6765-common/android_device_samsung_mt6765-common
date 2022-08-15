343d81e1c6886111fb049fc56f4a5be2a1395ebf
Matsvei Niaverau <matvej2002@gmail.com>
2022-08-15 17:36:58 +0300

    mt6765-common: power: Import power HAL extension
    * Added fts_ts touchscreen support
    
    * From https://github.com/LineageOS/android_device_xiaomi_surya/blob/c28fc6e3ebbc8467858beac9b268c3ade9df1cad/power/power-mode.cpp
    
    * Adapted for our Mediatek power HAL
    
    Change-Id: Ic2439c357aa45cc35940b0a14525d0034351d3ca
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>
    Signed-off-by: aayowtf <atharv2951@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -103,6 +103,9 @@ TARGET_COPY_OUT_ODM_DLKM := odm_dlkm
 BOARD_VENDOR := samsung
 TARGET_BOARD_PLATFORM := mt6765
 
+# Power
+TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
+
 # Properties
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/configs/props/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/configs/props/vendor.prop
