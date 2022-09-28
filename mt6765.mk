1bd38ca4934ab7a52a7d897bdecc5859c5450a35
Luca Stefani <luca.stefani.ge1@gmail.com>
2022-09-28 19:17:19 +0100

    mt6765-common: Enable updateable APEX
    Change-Id: Ic4d5feb869a57372f4dc8b9f8740dac150698b73
    Signed-off-by: aayowtf <atharv2951@proton.me>

--- mt6765.mk
+++ mt6765.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/samsung/mt6765-common/mt6765-common-vendor.mk)
 
