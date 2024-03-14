82a287b4a475bd6a2d8aa7e249eaa3e452e58a90
aayowtf <atharv2951@proton.me>
2024-03-14 06:07:28 +0530

    mt6765-common: Define Shipping API level
    Signed-off-by: aayowtf <atharv2951@proton.me>

--- mt6765.mk
+++ mt6765.mk
@@ -7,6 +7,9 @@
 # Inherit the proprietary files
 $(call inherit-product, vendor/samsung/mt6765-common/mt6765-common-vendor.mk)
 
+# API
+PRODUCT_SHIPPING_API_LEVEL := 31
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
